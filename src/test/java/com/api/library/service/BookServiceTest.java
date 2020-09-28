f4958dfc1ceb7b4b18664d81ac09e1e91351bfe9
Maiara Goldacker <maiaragoldacker@gmail.com>
2020-09-28 13:50:50 -0300

    delete service test

--- src/test/java/com/api/library/service/BookServiceTest.java
+++ src/test/java/com/api/library/service/BookServiceTest.java
@@ -109,6 +109,39 @@ public class BookServiceTest {
 		assertThat(book.isPresent()).isFalse();	
 	}
 	
+	@Test
+	@DisplayName("Deve deletar um livro")
+	public void deleteBookTest() {
+		Long id = 1L;
+		
+		//cenario
+		Book book = Book.builder().id(id).isbn("123").author("Fulano").title("As aventuras").build();
+		
+		//execução
+		//para garantir que não foi lançado nenhum erro
+		org.junit.jupiter.api.Assertions.assertDoesNotThrow(() -> service.delete(book));
+		
+		//verificações
+		Mockito.verify(repository, Mockito.times(1)).delete(book);
+	}
+
+	@Test
+	@DisplayName("Deve dar erro ao tentar deletar um livro inexistente")
+	public void deleteInexistentBookTest() {
+		
+		Book book = createValidBook();
+		
+		//criei o a instância de livro mas não salvei
+		Throwable exception = Assertions.catchThrowable(() ->service.delete(book));
+		assertThat(exception).isInstanceOf(IllegalArgumentException.class)
+							 .hasMessage("book id cant be null");
+		
+		org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, 
+																	() ->  service.delete(book));
+		
+		Mockito.verify(repository, Mockito.never()).delete(book);
+	}
+
 	public Book createValidBook() {
 		return Book.builder().author("Artur")
 				   .title("As Aventuras")
